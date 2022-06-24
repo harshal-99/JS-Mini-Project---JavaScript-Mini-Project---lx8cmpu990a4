8befa9071d53d9eaab78f54afca120721b545082
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-24 17:26:22 +0530

    Initial commit

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,15 +1,25 @@
 import styles from './Grid.module.css'
 import CountryCard from "./CountryCard";
+import {Link} from "react-router-dom";
 
 const Grid = ({countries}) => {
 	return (
 		<div className={styles["main-grid"]}>
 			{countries.map(country =>
-				<CountryCard region={country.region} population={country.population}
-				             name={country.name.common} capital={country.capital[0]}
-				             flag={country.flags.png} key={country.name.common}/>)}
+				<Link to={`/${country.name.common}`} key={country.name.common}>
+					<CountryCard region={country.region} population={country.population}
+					             name={country.name.common} capital={country.capital[0]}
+					             flag={country.flags.png} key={country.name.common}/>
+				</Link>
+			)}
 		</div>
 	)
 }
 
 export default Grid
+
+/*
+
+				</Link>
+
+ */

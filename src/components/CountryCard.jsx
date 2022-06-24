b7a5a75cfb521d28b961e3a9237eedc022f436be
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-24 17:26:02 +0530

    Initial commit

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -5,10 +5,10 @@ const CountryCard = ({name, flag, population, region, capital}) => {
 		<div className={styles.container}>
 			<img src={flag} alt={`${name} flag`}/>
 			<div className={styles.data}>
-				<div>{name}</div>
-				<p>Population: {population}</p>
-				<p>Region: {region}</p>
-				<p>Capital: {capital}</p>
+				<h3>{name}</h3>
+				<p><span className={styles.bold}>Population:</span> {population}</p>
+				<p><span className={styles.bold}>Region:</span> {region}</p>
+				<p><span className={styles.bold}>Capital:</span> {capital}</p>
 			</div>
 		</div>
 	)

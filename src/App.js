cf01c02186518582f20151130f804e2f97b60a01
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-28 09:54:15 +0530

    Implement Light/Dark mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import {useContext, useEffect, useState} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
 import styles from './App.module.css';
 import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
 import Filter from "./components/Filter";
-import {useEffect, useState} from "react";
 import axios from "axios";
 import Grid from "./components/Grid";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Country from "./routes/Country";
+import {ThemeContext} from "./ThemeContext";
+
 
 function App() {
 	const url = 'https://restcountries.com/v3.1/all'
 	const [countries, setCountries] = useState([])
 	const [searchInput, setSearchInput] = useState('')
 	const [region, setRegion] = useState('none')
+	const theme = useContext(ThemeContext)
+	const darkMode = theme.state.darkMode
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -51,12 +56,12 @@ function App() {
 	}
 
 	return (
-		<div className={styles.app}>
+		<div className={darkMode ? 'dark-background': 'light-background'}>
 			<NavBar/>
 			<BrowserRouter>
 				<Switch>
 					<Route exact path='/'>
-						<main>
+						<main className={darkMode ? 'dark-background': 'light-background'}>
 							<div className={styles.container}>
 								<SearchBar searchText={searchInput}
 								           updateSearchText={setSearchInput}/>
@@ -64,14 +69,14 @@ function App() {
 							</div>
 							<div className={styles.gridContainer}>
 								{(countriesToRender.length > 0)
-									? <Grid countries={countriesToRender}/>
+									? <Grid countries={countriesToRender} darkMode={darkMode}/>
 									: <div>Loading...</div>}
 							</div>
 						</main>
 					</Route>
 					<Route path="/:name"
 					       children={(countries.length > 0)
-						       ? <Country countries={countries}/>
+						       ? <Country countries={countries} darkMode={darkMode}/>
 						       : <div>Loading...</div>}/>
 				</Switch>
 			</BrowserRouter>

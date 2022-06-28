73dd630d5a3217c5d50ba2a63f7505f315f89a31
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-28 09:52:59 +0530

    Add dark/light mode classes

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,15 +1,18 @@
+import {Link} from "react-router-dom";
+import cx from "classnames";
+
 import styles from './Grid.module.css'
 import CountryCard from "./CountryCard";
-import {Link} from "react-router-dom";
 
-const Grid = ({countries}) => {
+const Grid = ({countries, darkMode}) => {
+
 	return (
-		<div className={styles["main-grid"]}>
+		<div className={cx(styles["main-grid"], darkMode ? 'dark-background': 'light-background')}>
 			{countries.map(country =>
 				<Link to={`/${country.name.common}`} key={country.name.common}>
 					<CountryCard region={country.region} population={country.population}
 					             name={country.name.common} capital={country.capital[0]}
-					             flag={country.flags.png} key={country.name.common}/>
+					             flag={country.flags.png} key={country.name.common} darkMode={darkMode}/>
 				</Link>
 			)}
 		</div>

b2fea788680ab812688e37a197aeed6ebefe0951
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-28 09:53:52 +0530

    Add dark/light mode classes

--- src/routes/Country.jsx
+++ src/routes/Country.jsx
@@ -1,9 +1,12 @@
 import {Link, useParams} from "react-router-dom";
+import cx from "classnames";
+
 import styles from "./Country.module.css"
 
-const Country = ({countries}) => {
+const Country = ({countries, darkMode}) => {
 	const {name} = useParams()
 	const country = countries.find(country => country.name.common === name)
+
 	if (!country) {
 		return <div>No Country Found</div>
 	}
@@ -22,39 +25,49 @@ const Country = ({countries}) => {
 		languages.push(country.languages[language])
 	}
 	return (
-		<div>
-			<Link to="/" style={{margin: "2rem"}}>Back</Link>
-			<div className={styles.container}>
+		<div style={{width: '100vw', height: '100vh'}}>
+			<Link to="/" style={{margin: "2rem"}} className={darkMode ? 'dark-text' : 'light-text'}>Back</Link>
+			<div className={cx(styles.container, darkMode ? 'dark-background' : 'light-background')}>
 				<img src={country.flags.png} alt={`${country.flags.png} flag`}/>
-				<div className={styles.info}>
-					<h2>{country.name.common}</h2>
+				<div className={cx(styles.info, darkMode ? 'dark-background' : 'light-background')}>
+					<h2 className={darkMode ? 'dark-text' : 'light-text'}>{country.name.common}</h2>
 					<div className={styles["more-info"]}>
 						<div>
-							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Native Name:</span> {nativeName.join(', ')}</div>
+							<div className={cx(styles["info-detail"], darkMode ? 'dark-background' : 'light-background')}>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Native Name:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {nativeName.join(', ')}</span></div>
 							</div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Population:</span> {country.population}</div>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Population:</span><span
+									className={darkMode ? 'dark-text' : 'light-text'}> {country.population}</span></div>
 							</div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Region:</span> {country.region}</div>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Region:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {country.region}</span></div>
 							</div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Sub Region:</span> {country.subregion}</div>
+								<div><span
+									className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Sub Region:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {country.subregion}</span>
+								</div>
 							</div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Capital:</span> {country.capital.join(', ')}</div>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Capital:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {country.capital.join(', ')}</span></div>
 							</div>
 						</div>
 						<div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Top Level Domain:</span> {country.tld[0]}</div>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Top Level Domain:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {country.tld[0]}</span></div>
 							</div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Currencies:</span> {currencies.join(', ')}</div>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Currencies:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {currencies.join(', ')}</span></div>
 							</div>
 							<div className={styles["info-detail"]}>
-								<div><span className={styles.bolder}>Languages:</span> {languages.join(', ')}</div>
+								<div><span className={cx(styles.bolder, darkMode ? 'dark-text' : 'light-text')}>Languages:</span>
+									<span className={darkMode ? 'dark-text' : 'light-text'}> {languages.join(', ')}</span></div>
 							</div>
 						</div>
 					</div>

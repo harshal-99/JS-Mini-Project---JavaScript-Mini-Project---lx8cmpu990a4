2008ad77e5dd735a287b77fd892f5a936c28f2a3
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-28 09:52:38 +0530

    Add dark/light mode classes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,28 @@
 import styles from './NavBar.module.css'
+import '../index.css'
+import {useContext} from "react";
+import {ThemeContext} from "../ThemeContext";
+import cx from "classnames"
+
 
 const NavBar = () => {
+	const theme = useContext(ThemeContext)
+	const darkMode = theme.state.darkMode
+	const changeTheme = () => {
+		if (darkMode) {
+			theme.dispatch({type: 'LIGHT_MODE'})
+		} else {
+			theme.dispatch({type: 'DARK_MODE'})
+		}
+	}
 	return (
-		<nav>
-			<div className={styles.padding}>Where in the World</div>
-			<div className={styles.padding}>Dark Mode</div>
+		<nav className={darkMode ? 'dark-elements' : 'light-elements'}>
+			<div className={cx(styles.padding, styles['font-weight-600'], darkMode ? 'dark-text' : 'light-text')}>
+				Where in the World
+			</div>
+			<button className={cx(styles.padding, darkMode ? 'dark-text' : 'light-text')} onClick={changeTheme}>
+				{darkMode ? 'Light Mode' : 'Dark Mode'}
+			</button>
 		</nav>
 	)
 }

0f221d3c4aaaeee055e0d7c55d56575b42bd6c8a
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-28 09:53:12 +0530

    Add dark/light mode classes

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,14 +1,22 @@
 import styles from './CountryCard.module.css'
+import cx from "classnames";
 
-const CountryCard = ({name, flag, population, region, capital}) => {
+const CountryCard = ({name, flag, population, region, capital, darkMode}) => {
 	return (
-		<div className={styles.container}>
+		<div className={cx(styles.container, darkMode ? 'dark-background' : 'light-background')}>
 			<img src={flag} alt={`${name} flag`}/>
 			<div className={styles.data}>
-				<h3>{name}</h3>
-				<p><span className={styles.bold}>Population:</span> {population}</p>
-				<p><span className={styles.bold}>Region:</span> {region}</p>
-				<p><span className={styles.bold}>Capital:</span> {capital}</p>
+				<div className={cx(styles['font-weight-600'], darkMode ? 'dark-text' : 'light-text')}>{name}</div>
+				<p><span
+					className={cx(styles['font-weight-600'], darkMode ? 'dark-text' : 'light-text')}>Population:</span> <span
+					className={darkMode ? 'dark-text' : 'light-text'}>{population}</span>
+				</p>
+				<p><span
+					className={cx(styles['font-weight-600'], darkMode ? 'dark-text' : 'light-text')}>Region:</span> <span
+					className={darkMode ? 'dark-text' : 'light-text'}>{region}</span></p>
+				<p><span
+					className={cx(styles['font-weight-600'], darkMode ? 'dark-text' : 'light-text')}>Capital:</span> <span
+					className={darkMode ? 'dark-text' : 'light-text'}>{capital}</span></p>
 			</div>
 		</div>
 	)
